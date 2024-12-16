2a58a911affd2cfb5caba89ab82ea79eee5fa9d7
jgreiffo <jgreiffo@gmail.com>
2024-12-16 10:02:42 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ const question = document.querySelectorAll('ul li');
 console.log(questions);
 const arrow = document.selectElementById('arrowDown');
 console.log(arrow);
+
+const question1 = document.querySelector('question1')
+question1.style.backgroundColor='red';
+question1.style.color='#6dff00';
