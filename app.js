52517319e296d28b42bcabae0dcf3b5d34e7f00e
jgreiffo <jgreiffo@gmail.com>
2024-12-16 08:49:29 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -2,5 +2,7 @@
 // console.log(questions);
 const questions = document.getElementsByTagName('li');
 console.log(questions);
-const question = document.querySelector('questions');
+const question = document.querySelectorAll('ul li');
 console.log(questions);
+const arrow = document.selectElementById('arrowDown');
+console.log(arrow);
