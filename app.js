5b4d67546b4a15f02ed9cb2e3465ecbd9f8676e1
jgreiffo <jgreiffo@gmail.com>
2024-12-16 10:14:20 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 // const questions = document.getElementsByClassName('question');
 // console.log(questions);
-const questions = document.getElementsByTagName('li');
-console.log(questions);
-const question = document.querySelectorAll('ul li');
-console.log(questions);
-const arrow = document.selectElementById('arrowDown');
-console.log(arrow);
+// const questions = document.getElementsByTagName('li');
+// console.log(questions);
+// const question = document.querySelectorAll('ul li');
+// console.log(questions);
+// const arrow = document.selectElementById('arrowDown');
+// console.log(arrow);
 
-const question1 = document.querySelector('question1')
-question1.style.backgroundColor='red';
-question1.style.color='#6dff00';
+// const question1 = document.querySelector('question1')
+// question1.style.backgroundColor='red';
+// question1.style.color='#6dff00';
+
+const slides = document.querySelectorAll('.slide');
+        let currentSlide = 0;
+
+        const prevButton = document.getElementById('prevButton');
+        const nextButton = document.getElementById('nextButton');
